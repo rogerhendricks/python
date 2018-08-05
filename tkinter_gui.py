b28d27ae2ea06d53d023513776db9fa0267f4d90
Roger Hendricks <hendricks.r@gmail.com>
2018-08-05 22:42:16 +1000

    new

---
+++ tkinter_gui.py
@@ -0,0 +1,16 @@
+from tkinter import *
+# main window
+mainWindow = Tk()
+
+# create a top container
+topFrame = Frame(mainWindow)
+topFrame.pack()
+# create a bottom container
+bottomFrame = Frame(mainWindow)
+
+
+# infinite loop to keep it open until closed button is pushed.
+
+mainWindow.mainloop()
+
+
